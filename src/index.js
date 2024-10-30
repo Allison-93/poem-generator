a289a453ab61e9c692cec483e463cd2fa0ec097d
Allison-93 <allisonmoopanar@gmail.com>
2024-10-30 13:58:08 +0200

    Added basic form handling and typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generatePoem(event) {
+  event.preventDefault();
+  new Typewriter("#poem", {
+    strings: "This is a special poem just for you",
+    autoStart: true,
+    delay: 1,
+    cursor: null,
+  });
+}
 
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
